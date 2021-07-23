7436810feb5f540cfb487750234b7f32cbcc1998
Toni Naumanen <toni.naumis@gmail.com>
2021-07-23 13:40:41 +0300

    refactored "me" route

--- src/main/java/taskTracker/auth/AuthController.java
+++ src/main/java/taskTracker/auth/AuthController.java
@@ -2,9 +2,13 @@ package taskTracker.auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import taskTracker.appuser.AppUser;
 import taskTracker.jwt.JwtTokenUtil;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping(path = "/auth")
@@ -30,5 +34,15 @@ public class AuthController {
     AppUser appUser = authService.loginUser(authRequest);
     AuthResponse authResponse = new AuthResponse(jwtTokenUtil.generateToken(appUser));
     return authResponse;
+  }
+
+  @GetMapping("/me")
+  @ResponseStatus(HttpStatus.OK)
+  public Map<String, Object> me(Authentication authentication) {
+    AppUser appUser = (AppUser) authentication.getPrincipal();
+    Map<String, Object> fields = new HashMap<>();
+    fields.put("id", appUser.getId());
+    fields.put("username", appUser.getUsername());
+    return fields;
   }
 }
