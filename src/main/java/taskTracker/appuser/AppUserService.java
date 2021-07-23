c61858c04b24eb0db081c511236a8752ff1c6668
Toni Naumanen <toni.naumis@gmail.com>
2021-07-23 14:04:52 +0300

    refactored appuser

--- src/main/java/taskTracker/appuser/AppUserService.java
+++ src/main/java/taskTracker/appuser/AppUserService.java
@@ -1,28 +1,16 @@
 package taskTracker.appuser;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
 
 @Service
 public class AppUserService implements UserDetailsService {
 
   @Autowired
   private AppUserRepository appUserRepository;
-  @Autowired
-  private BCryptPasswordEncoder bCryptPasswordEncoder;
-  @Autowired
-  private AuthenticationProvider authenticationProvider;
 
   @Override
   public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
@@ -31,25 +19,8 @@ public class AppUserService implements UserDetailsService {
             new UsernameNotFoundException(String.format("username: %s, not found", s)));
   }
 
-  @Transactional
-  public AppUser registerUser(AppUser appUser) {
-    Optional<AppUser> user = appUserRepository.findByUsername(appUser.getUsername());
-    if (user.isPresent()) {
-      throw new IllegalStateException(String.format("username: %s, is taken", appUser.getUsername()));
-    }
-    String encodedPassword = bCryptPasswordEncoder.encode(appUser.getPassword());
-    appUser.setPassword(encodedPassword);
-    return appUserRepository.save(appUser);
-  }
-
-  @Transactional
-  public AppUser loginUser(AppUser appUser) {
-    try {
-      Authentication auth = authenticationProvider.authenticate(
-          new UsernamePasswordAuthenticationToken(appUser.getUsername(), appUser.getPassword()));
-      return (AppUser) auth.getPrincipal();
-    } catch (Exception e) {
-      throw new BadCredentialsException(e.getMessage());
-    }
+  public AppUser findUser(Long id) {
+    return appUserRepository.findById(id)
+        .orElseThrow(() -> new AppUserNotFoundException(id));
   }
 }
--- src/main/java/taskTracker/exception/GlobalExceptionHandler.java
+++ src/main/java/taskTracker/exception/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import taskTracker.appuser.AppUserNotFoundException;
 import taskTracker.task.TaskNotFoundException;
 import taskTracker.taskgroup.TaskGroupNotFoundException;
 
@@ -17,7 +18,7 @@ import java.util.Map;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-  @ExceptionHandler({TaskGroupNotFoundException.class, TaskNotFoundException.class})
+  @ExceptionHandler({TaskGroupNotFoundException.class, TaskNotFoundException.class, AppUserNotFoundException.class})
   @ResponseStatus(HttpStatus.NOT_FOUND)
   @ResponseBody
   ExceptionResponse notFoundHandler(HttpServletRequest req, RuntimeException e) {
